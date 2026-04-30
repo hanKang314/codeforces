ec4a6b559b9d545e5fc1d20a0f9aae1ad64e4672
hanKang314 <provento@snu.ac.kr>
2026-04-30 16:53:24 +0900

    cf1095d2 D

--- cf1095d2/D.cpp
+++ cf1095d2/D.cpp
@@ -2,28 +2,77 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    int t;
+    cin >> t;
 
-    int T;
-    cin >> T;
-
-    while (T--) {
+    while (t--) {
         int n;
         cin >> n;
 
         vector<int> a(n);
         for (int i = 0; i < n; i++) cin >> a[i];
 
-        vector<int> sa = a;
-        sort(sa.begin(), sa.end());
-
-        for (int i = 0; i < n; i++) {
-            
+        vector<int> even, odd;
+        for (auto& z : a) {
+            if (z % 2) odd.push_back(z);
+            else even.push_back(z);
         }
 
+        int even_min = 1e9;
+        int even_max = -1e9;
+        int odd_min = 1e9;
+        int odd_max = -1e9;
 
-        
-        
+        if (!even.empty()) {
+            for (auto& z : even) {
+                even_min = min(even_min, z);
+                even_max = max(even_max, z);
+            }
+        }
+
+        if (!odd.empty()) {
+            for (auto& z : odd) {
+                odd_min = min(odd_min, z);
+                odd_max = max(odd_max, z);
+            }
+        }
+
+        bool is = 1;
+
+        if (even.size() > 1) {
+            int mx = even[0];
+
+            for (int i = 1; i < even.size(); i++) {
+                if (mx > even[i]) {
+                    if (odd_min > odd[i] && odd_max < mx) {
+                        is = 0;
+                        break;
+                    }
+                }
+                else {
+                    mx = even[i];
+                }
+            }
+        }
+
+        if (odd.size() > 1) {
+            int mx = odd[0];
+
+            for (int i = 1; i < odd.size(); i++) {
+                if (mx > odd[i]) {
+                    if (even_min > odd[i] && even_max < mx) {
+                        is = 0;
+                        break;
+                    }
+                }
+                else {
+                    mx = odd[i];
+                }
+            }
+        }
+
+        cout << (is ? "YES" : "NO") << '\n';
     }
+
+    return 0;
 }
